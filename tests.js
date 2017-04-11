5ee48c89991a281d3b0408f96f9a70dc60e3eccc
RyleeAndrews <ryleeandrews73@gmail.com>
2017-04-11 11:56:54 -0700

    ready to turn in

--- tests.js
+++ tests.js
@@ -1,5 +1,4 @@
 'use strict';
-console.log('is this working');
 function testSum() { //eslint-disable-line
   if (sum(4,7)[1] === 'The sum of 4 and 7 is 11.') {
     console.log('%c TEST FOR sum() PASSES', 'color: green');
